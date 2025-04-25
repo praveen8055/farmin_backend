0ebd46483e23170b68ccaaedd80c7cf9615e34dd
praveen8055 <88497498+praveen8055@users.noreply.github.com>
2025-04-25 18:17:58 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,1 @@
-module.exports = { apps : [{ name: 'farmin_backend', script: 'npm', args: 'run server', env: { NODE_ENV: 'development', ENV_VAR1: 'environment-variable', } }], };
+module.exports = { apps : [{ name: 'farmin-backend', script: 'npm', args: 'run server', env: { NODE_ENV: 'development', ENV_VAR1: 'environment-variable', } }], };
